5ea1be3b6f19ace970da01c7bbd4e11407fbeb5e
Mimi Zhang <MimiZhang2027@u.northwestern.edu>
2026-01-27 15:23:48 -0600

    walk tested over untested

--- asn1_x/asn1_walk.py
+++ asn1_x/asn1_walk.py
@@ -3,7 +3,6 @@ import sonar
 from asn1_turn_right import turnRight90
 from asn1_turn_left import turnLeft90
 from asn1_movement import sensor_left, sensor_right, sensor_reset
-import time
 from asn1_initialization import initialization
 
 s = sonar.Sonar()
@@ -14,7 +13,7 @@ def blocked(dist):
         return True
     else:
         return False
-'''
+
 def checkBlockedandRotate():
     print("checking")
     print(s.getDistance())
@@ -51,40 +50,40 @@ def checkBlockedandRotate():
                 print("blocked left")
                 turnRight90()
     sensor_reset()
-'''
 
-def checkBlockedandRotate():
-    print("checking")
-    print(s.getDistance())
-    right_blocked = False
-    left_blocked = False
+## UNTESTED CODE THAT IS BETTER
+# def checkBlockedandRotate():
+#     print("checking")
+#     print(s.getDistance())
+#     right_blocked = False
+#     left_blocked = False
 
-    # Check if blocked forward
-    if blocked(s.getDistance()):
-        print("blocked front")
+#     # Check if blocked forward
+#     if blocked(s.getDistance()):
+#         print("blocked front")
 
-        # Rotate sensor right & check right distance
-        sensor_right()
-        print("right distance: " + str(s.getDistance()))
-        if blocked(s.getDistance()):
-            right_blocked = True
+#         # Rotate sensor right & check right distance
+#         sensor_right()
+#         print("right distance: " + str(s.getDistance()))
+#         if blocked(s.getDistance()):
+#             right_blocked = True
         
-        # Rotate sensor left & check left distance
-        sensor_left()
-        print("left distance: " + str(s.getDistance()))
-        if blocked(s.getDistance()):
-            left_blocked = True
+#         # Rotate sensor left & check left distance
+#         sensor_left()
+#         print("left distance: " + str(s.getDistance()))
+#         if blocked(s.getDistance()):
+#             left_blocked = True
 
-        # turn based on left & right blocks
-        if right_blocked and left_blocked:
-            turnRight90()
-            turnRight90()
-        elif right_blocked:
-            turnLeft90()
-        else:
-            turnRight90()
+#         # turn based on left & right blocks
+#         if right_blocked and left_blocked:
+#             turnRight90()
+#             turnRight90()
+#         elif right_blocked:
+#             turnLeft90()
+#         else:
+#             turnRight90()
         
-    sensor_reset()
+#     sensor_reset()
 
 if __name__ == '__main__':  
     initialization()
