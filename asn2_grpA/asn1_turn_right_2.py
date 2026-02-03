8368556fdf69c11cd75ae94e7044cd8a2688ba55
Mimi Zhang <MimiZhang2027@u.northwestern.edu>
2026-02-03 14:51:44 -0600

    playing with turns

--- asn2_grpA/asn1_turn_right_2.py
+++ asn2_grpA/asn1_turn_right_2.py
@@ -6,73 +6,42 @@ from asn1_movement import makeStep, send_positions
 import time
 
 OG_HIP = 500
-
-def firstMiniTurnRight():
+TURN_HIP = 350
+def turnRight():
     # Step 1: Lift and move tripod group A where legs 1 & 3 move forwards & 5 moves backwards 
     step1 = makeStep(
-        hip = {1:400, 3:OG_HIP, 5:400, 2:OG_HIP, 4:OG_HIP, 6:OG_HIP},
-        knee = {1:200, 3:200, 5:700}
+        hip = {1:TURN_HIP, 3:TURN_HIP, 5:TURN_HIP, 2:OG_HIP, 4:OG_HIP, 6:OG_HIP},
+        knee = {1:213, 3:220, 5:786}
     )
     send_positions(.3, step1) 
-    time.sleep(.3)
-
-def lastMiniTurnRight():
-    # Step 3: Lift and move tripod group B where legs 2 move forwards & legs 4 & 6 move backwards
-    step3 = makeStep(
-        hip = {1:OG_HIP, 3:OG_HIP, 5:OG_HIP, 2:400, 4:OG_HIP, 6:400},
-        knee = {2:180, 4:780, 6:750 }
-    )
-
-    send_positions(.3, step3)
-    time.sleep(.3)
-
-def turnRight(firstMiniTurn, lastMiniTurn):
-    if (firstMiniTurn):
-        firstMiniTurnRight()
-    else: 
-        # Step 1: Lift and move tripod group A where legs 1 & 3 move forwards & 5 moves backwards 
-        step1 = makeStep(
-            hip = {1:400, 3:400, 5:400, 2:OG_HIP, 4:OG_HIP, 6:OG_HIP},
-            knee = {1:200, 3:200, 5:700}
-        )
-        send_positions(.3, step1) 
-        time.sleep(.3)
+    time.sleep(2)
     
     # Step 2: Put down Tripod Group A
     step2 = makeStep(
-        knee = {1: 280, 3:280, 5:650}
+        knee = {1: 313, 3: 320, 5: 686},
     )
     send_positions(.2, step2)
-    time.sleep(.3)
-
-    if (lastMiniTurn):
-        lastMiniTurnRight()
-    else:
-        # Step 3: Lift and move tripod group B where legs 2 move forwards & legs 4 & 6 move backwards
-        step3 = makeStep(
-            hip = {1:OG_HIP, 3:OG_HIP, 5:OG_HIP, 2:400, 4:400, 6:400},
-            knee = {2:180, 4:780, 6:750 }
-        )
-
+    time.sleep(2)
+    
+    # Step 3: Lift and move tripod group B where legs 2 move forwards & legs 4 & 6 move backwards
+    step3 = makeStep(
+        hip = {1:OG_HIP, 3:OG_HIP, 5:OG_HIP, 2:TURN_HIP, 4:TURN_HIP, 6:TURN_HIP},
+        knee = {2:212, 4:786, 6:786}
+    )
     send_positions(.3, step3)
-    time.sleep(.3)
+    time.sleep(2)
 
     # Step 4: Put tripod group B back on the ground
     step4 = makeStep(
-        knee = {2:270, 4:690, 6:690}
+        knee = {2: 312, 4: 686, 6: 686},
     )
 
     send_positions(.2, step4)
-    time.sleep(.3)
+    time.sleep(2)
 
 def turnRight90():
     for i in range(4):
-        if i == 0:
-            turnRight(True, False)
-        if i == 3:
-            turnRight(False, True)
-        else:
-            turnRight(False, False)
+        turnRight()
     
     # Return tripod B back to beginning
     step5 = makeStep(
