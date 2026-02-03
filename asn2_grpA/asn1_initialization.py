eee90ee4f326d0011a4af792aff2c7378cc832a7
EmilyChi5 <emily.chi12232@gmail.com>
2026-02-03 14:27:39 -0600

    Update asn1_initialization.py

--- asn2_grpA/asn1_initialization.py
+++ asn2_grpA/asn1_initialization.py
@@ -1,4 +1,4 @@
-from asn1_movement import send_positions, makeStep
+from asn1_movement import send_positions, makeStep, sensor_reset
 import signal
 import time
 OG_HIP = 500
@@ -15,17 +15,30 @@ def initialization ():
     sensor_reset()
     print("Initializing")
     
-    print("Setting Hips")
-    send_positions(1, makeStep(hip = {1: OG_HIP, 2: OG_HIP, 3: OG_HIP, 4: OG_HIP, 5: OG_HIP, 6: OG_HIP}))
+    print("Setting Tripod Group A")
+    step1 = makeStep(
+        hip = {1: OG_HIP, 3: OG_HIP, 5: OG_HIP},
+        knee = {1: 313, 3: 320, 5: 686},
+        foot = {1: 200, 3: 200, 5: 800}
+    )
+    send_positions(1, step1)
     time.sleep(1)
 
-    print("Setting Knees")
-    send_positions(1, makeStep(knee = {1: 313, 2: 312, 3: 320, 4: 686, 5: 686, 6: 686}))
+    print("Setting Tripod Group B")
+    step2 = makeStep(
+        hip = {2: OG_HIP,4: OG_HIP, 6: OG_HIP},
+        knee = {2: 312, 4: 686, 6: 686},
+        foot = {2: 199, 4: 799, 6: 799}
+    )
+    send_positions(1, step2)
     time.sleep(1)
 
-    print("Setting Feet")
-    send_positions(1, makeStep(foot = {1: 200, 2: 199, 3: 200, 4: 799, 5: 800, 6: 799}))
-    time.sleep(1)
+    # print("Setting Feet")
+    # step3 = makeStep(
+    #     foot = {1: 200, 2: 199, 3: 200, 4: 799, 5: 800, 6: 799}
+    # )
+    # send_positions(1, step3)
+    # time.sleep(1)
     
     print("Finished Initialization")
 
